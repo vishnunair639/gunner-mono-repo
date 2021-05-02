7e1a85f9f69fc22d1a405e48f70c59422f3f000a
COG <apple@COG-MBP.local>
2021-05-02 22:35:13 +0530

    Creating a mono repo for all gunner reated works

---
+++ apps/history/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gunnerwhocodes-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'history';
+}
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  projects: ['<rootDir>/apps/gunners'],
+  projects: ['<rootDir>/apps/gunners', '<rootDir>/apps/history'],
 };
